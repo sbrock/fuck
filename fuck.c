24a3283311e846499ba2b1f290fb725e361d833f
madmage <madmage@inipi.org>
2021-08-23 09:41:58 -0400

    add fuck.c

---
+++ fuck.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+// fuck.c  internet: serious fucking business!
+
+main(int argc, char * argv[] )
+{
+	printf("\n%s: WOW, you use some foul language!!!\nDo you kiss your mother with that mouth?!!\n\n",argv[0]);
+	return 0;
+}
+
